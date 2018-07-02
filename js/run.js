f84173fc8376f821cd22621de1cd5aff93896656
Eyal Soha <eyalsoha@gmail.com>
2018-07-02 13:15:56 +0300

    Use JSON stringify.  This fixes https://github.com/eyal0/OctoPrint-PrintTimeGenius/issues/11

--- js/run.js
+++ js/run.js
@@ -1,47 +1,44 @@
-var Worker = require('tiny-worker');
-var fs = require('fs');
+let Worker = require('tiny-worker');
+let fs = require('fs');
 
 gcodeLines = [];
 
 settings = {"maxSpeed":[100,100,10,100],"maxPrintAcceleration":[1000,1000,100,10000],"maxTravelAcceleration":[1000,1000,100,10000],"maxJerk":[10,10,1,10],"absoluteExtrusion":false,"feedrateMultiplyer":100,"filamentDiameter":1.75,"firmwareRetractLength":2,"firmwareUnretractLength":2,"firmwareRetractSpeed":50,"firmwareUnretractSpeed":50,"firmwareRetractZhop":0,"timeScale":1.01}
 
-var gcodeProcessorWorker = new Worker(__dirname + '/gcodeProcessor.js');
-var progress = [];
+let gcodeProcessorWorker = new Worker(__dirname + '/gcodeProcessor.js');
+let progress = [];
+let result = {};
 gcodeProcessorWorker.onmessage = function (e) {
   if ("filepos" in e.data) {
     progress.push([e.data.filepos, e.data.printTime])
   }
   if ("result" in e.data) {
-    //console.log(e.data)
     // All the data is in progress now.
-    var total_filesize = progress[progress.length-1][0]
-    var total_printtime = progress[progress.length-1][1]
-    console.log("{\"progress\": [")
-    console.log("[0,0],")
-    var last_printed_progress = 0;
+    let total_filesize = progress[progress.length-1][0]
+    let total_printtime = progress[progress.length-1][1]
+    result["progress"] = []
+    result["progress"].push([0,0]);
+    let last_printed_progress = 0;
     for (progress_entry of progress) {
-      var new_printed_progress = progress_entry[1];
+      let new_printed_progress = progress_entry[1];
       if (last_printed_progress+60 < new_printed_progress) {
-        console.log("[" + progress_entry[0] +
-                    "," + progress_entry[1] +
-                    "],");
+        result["progress"].push([
+          progress_entry[0],
+          progress_entry[1]]);
         last_printed_progress = new_printed_progress;
       }
     }
-    console.log("[" + progress[progress.length-1][0] +
-                "," + progress[progress.length-1][1] +
-                "]");
-    console.log("]}");
+    result["progress"].push([
+      progress[progress.length-1][0],
+      progress[progress.length-1][1]]);
+    result["estimatedPrintTime"] = progress[progress.length-1][1];
+    // All done.
+    console.log(JSON.stringify(result));
     gcodeProcessorWorker.terminate();
   }
 }
 fs.readFile(process.argv[2], "utf8",
             function(err, data) {
               gcodeLines = data.split(/(?=[\r\n]+)/g);
-              //console.log(gcodeLines);
-              /*console.log(gcodeLines
-                          .map(x => x.length)
-                          .reduce((x,y) => x + y));*/
-
               gcodeProcessorWorker.postMessage([gcodeLines, settings]);
             });
