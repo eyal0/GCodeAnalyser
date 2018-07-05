24e6be66df32052af61406dac940bb2dfefb9eae
Eyal Soha <eyalsoha@gmail.com>
2018-07-05 11:31:12 +0300

    Add first and last filament into the output
    This is working on https://github.com/eyal0/OctoPrint-PrintTimeGenius/issues/18

--- js/analyse.js
+++ js/analyse.js
@@ -10,9 +10,11 @@ let progress = [];
 let result = {};
 const MIN_FILAMENT = 5;  // Don't record times for anything before the first 5mm of filament.
 let filesize;
+let totalFilamentUsed = Number.NEGATIVE_INFINITY;
 gcodeProcessorWorker.onmessage = function (e) {
   if ("filePosition" in e.data) {
-    progress.push(e.data)
+    progress.push(e.data);
+    totalFilamentUsed = e.data["filamentUsage"];
   }
   if ("result" in e.data) {
     // progress is a list of objects, each with filePosition, printTime, and filamentUsage
@@ -22,8 +24,20 @@ gcodeProcessorWorker.onmessage = function (e) {
     // All the data is in progress now.
     result["progress"] = []
     result["progress"].push([0, result["estimatedPrintTime"]]);
-    let last_printed_progress = 0;
+    let last_printed_progress = Number.NEGATIVE_INFINITY;
     for (progress_entry of progress) {
+      if (!('firstFilament' in result) && progress_entry["filamentUsage"] > 0) {
+        // Output the file position where the first filament comes out, which is probably after the heatup time.
+        result['firstFilament'] = progress_entry["filePosition"]/filesize;
+        // Force writing progress.
+        last_printed_progress = Number.NEGATIVE_INFINITY;
+      }
+      if (!('lastFilament' in result) && progress_entry["filamentUsage"] >= totalFilamentUsed) {
+        // Output the file position where the last filament comes out, which is probably the start of cooldown.
+        result['lastFilament'] = progress_entry["filePosition"]/filesize;
+        // Force writing progress.
+        last_printed_progress = Number.NEGATIVE_INFINITY;
+      }
       let new_printed_progress = progress_entry["printTime"];
       if (last_printed_progress+60 < new_printed_progress) {
         result["progress"].push([progress_entry["filePosition"]/filesize, totalPrintTime - progress_entry["printTime"]]);
